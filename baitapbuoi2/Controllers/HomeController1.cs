d588487659c103c2cd242d6dfd02cfbac60598aa
nguyenvandinh1000 <dinhnguyen19022005@gmail.com>
2025-08-29 08:21:19 +0700

    Add project files.

---
+++ baitapbuoi2/Controllers/HomeController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitapbuoi2.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Đây là trang giới thiệu.";
+            return View();
+        }
+    }
+}
---
+++ baitapbuoi2/Controllers/baitapbuoi2.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.text.Encodings.Web;
+
+
+namespace baitapbuoi2.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        public string Index()
+        {
+            return "this is my default action...";
+        }
+        public string Welcom()
+        {
+            return "this is welcom action method...";
+        }
+    }
+}
